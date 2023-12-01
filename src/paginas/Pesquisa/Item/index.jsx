07c2bbef9fd69262dcfee2145d4fe635c99ed1b0
GuiZin3829 <guilhermefragiacomo1@gmail.com>
2023-12-01 09:10:56 -0300

    atualizaca13

--- src/paginas/Pesquisa/Item/index.jsx
+++ src/paginas/Pesquisa/Item/index.jsx
@@ -25,7 +25,34 @@ export default function Item({ pers, veiculos, naves, planetas }) {
                         film.charAt(film.length - 2) + ", "
                     )}</h2>
                     <p>ID: {pers.url}</p>
-                    <p>{pers.crew}</p>
+                    <p>{`Tripulação ${pers.crew}`}</p>
+                    <p>{`Capacidade ${pers.cargo_capacity}`}</p>
+                    <p>{`${pers.height}`}</p>
+                    <p>{`${pers.mass}`}</p>
+                    <p>{`${pers.hair_color}`}</p>
+                    <p>{`${pers.skin_color}`}</p>
+                    <p>{`${pers.birth_year}`}</p>
+                    <p>{`${pers.gender}`}</p>
+                    <p>{`${pers.model}`}</p>
+                    <p>{`${pers.manufacturer}`}</p>
+                    <p>{`${pers.cost_in_credits}`}</p>
+                    <p>{`${pers.length}`}</p>
+                    <p>{`${pers.max_atmosphering_speed}`}</p>
+                    <p>{`${pers.passengers}`}</p>
+                    <p>{`${pers.consumables}`}</p>
+                    <p>{`${pers.starship_class}`}</p>
+                    "model": "CR90 corvette",
+                    "manufacturer": "Corellian Engineering Corporation",
+                    "cost_in_credits": "3500000",
+                    "length": "150",
+                    "max_atmosphering_speed": "950",
+                    "crew": "30-165",
+                    "passengers": "600",
+                    "cargo_capacity": "3000000",
+                    "consumables": "1 year",
+                    "hyperdrive_rating": "2.0",
+                    "MGLT": "60",
+                    "starship_class": "corvette",
                 </div>
             </div>
         </>
