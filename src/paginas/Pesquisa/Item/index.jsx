567d7aa8519a10a73b5b38a464f9a91daf340e7d
GuiZin3829 <guilhermefragiacomo1@gmail.com>
2023-12-01 09:19:14 -0300

    atualizaca15

--- src/paginas/Pesquisa/Item/index.jsx
+++ src/paginas/Pesquisa/Item/index.jsx
@@ -26,33 +26,21 @@ export default function Item({ pers, veiculos, naves, planetas }) {
                     )}</h2>
                     <p>ID: {pers.url}</p>
                     <p>{pers.crew == undefined ? "" : `Tripulação ${pers.crew}`}</p>
-                    <p>{pers.cargo_capacity === undefined ? "" : `Capacidade ${pers.cargo_capacity}`}</p>
-                    <p>{`${pers.height}`}</p>
-                    <p>{`${pers.mass}`}</p>
-                    <p>{`${pers.hair_color}`}</p>
-                    <p>{`${pers.skin_color}`}</p>
-                    <p>{`${pers.birth_year}`}</p>
-                    <p>{`${pers.gender}`}</p>
-                    <p>{`${pers.model}`}</p>
-                    <p>{`${pers.manufacturer}`}</p>
-                    <p>{`${pers.cost_in_credits}`}</p>
-                    <p>{`${pers.length}`}</p>
-                    <p>{`${pers.max_atmosphering_speed}`}</p>
-                    <p>{`${pers.passengers}`}</p>
-                    <p>{`${pers.consumables}`}</p>
-                    <p>{`${pers.starship_class}`}</p>
-                    "model": "CR90 corvette",
-                    "manufacturer": "Corellian Engineering Corporation",
-                    "cost_in_credits": "3500000",
-                    "length": "150",
-                    "max_atmosphering_speed": "950",
-                    "crew": "30-165",
-                    "passengers": "600",
-                    "cargo_capacity": "3000000",
-                    "consumables": "1 year",
-                    "hyperdrive_rating": "2.0",
-                    "MGLT": "60",
-                    "starship_class": "corvette",
+                    <p>{pers.cargo_capacity == undefined ? "" : `Capacidade ${pers.cargo_capacity}`}</p>
+                    <p>{pers.height == undefined ? "" : `${pers.height}`}</p>
+                    <p>{pers.mass == undefined ? "" : `${pers.mass}`}</p>
+                    <p>{pers.hair_color == undefined ? "" : `${pers.hair_color}`}</p>
+                    <p>{pers.skin_color == undefined ? "" : `${pers.skin_color}`}</p>
+                    <p>{pers.birth_year == undefined ? "" : `${pers.birth_year}`}</p>
+                    <p>{pers.gender == undefined ? "" : `${pers.gender}`}</p>
+                    <p>{pers.model == undefined ? "" : `${pers.model}`}</p>
+                    <p>{pers.manufacturer == undefined ? "" : `${pers.manufacturer}`}</p>
+                    <p>{pers.cost_in_credits == undefined ? "" : `${pers.cost_in_credits}`}</p>
+                    <p>{pers.length == undefined ? "" : `${pers.length}`}</p>
+                    <p>{pers.max_atmosphering_speed == undefined ? "" : `${pers.max_atmosphering_speed}`}</p>
+                    <p>{pers.passengers == undefined ? "" : `${pers.passengers}`}</p>
+                    <p>{pers.consumables == undefined ? "" : `${pers.consumables}`}</p>
+                    <p>{pers.starship_class == undefined ? "" : `${pers.starship_class}`}</p>
                 </div>
             </div>
         </>
